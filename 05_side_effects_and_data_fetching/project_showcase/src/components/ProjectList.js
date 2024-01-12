f7ac426b4bb14d62508e8fa7d0208d71adb40cb6
betalantz <betalantz@gmail.com>
2024-01-12 12:11:53 -0800

    delete faulty export

--- 05_side_effects_and_data_fetching/project_showcase/src/components/ProjectList.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/components/ProjectList.js
@@ -1,19 +1,18 @@
 import { useState } from 'react';
 import ProjectListItem from "./ProjectListItem";
 
-const ProjectList = ({ projects, onLoadProjects }) => {
+const ProjectList = ({ projects, onSelectPhase, searchQuery, onSearchChange, selectedPhase }) => {
 
-  const [searchQuery, setSearchQuery] = useState("")
 
   const handleSearch = (e) => {
-    setSearchQuery(e.target.value)
+    onSearchChange(e.target.value)
   }
 
-  const searchResults = projects.filter(project => {
-    return project.name.toLowerCase().includes(searchQuery.toLowerCase())
-  })
+  // const searchResults = projects.filter(project => {
+  //   return project.name.toLowerCase().includes(searchQuery.toLowerCase())
+  // })
 
-  const projectListItems = searchResults.map(project => (
+  const projectListItems = projects.map(project => (
     <ProjectListItem
       key={project.id}
       {...project}
@@ -22,21 +21,21 @@ const ProjectList = ({ projects, onLoadProjects }) => {
 
   return (
     <section>
-      <button onClick={onLoadProjects}>Load Projects</button>
       <h2>Projects</h2>
 
       <div className="filter">
-        <button>All</button>
-        <button>Phase 5</button>
-        <button>Phase 4</button>
-        <button>Phase 3</button>
-        <button>Phase 2</button>
-        <button>Phase 1</button>
+        <button className={selectedPhase === "" ? "active" : ""} onClick={()=>onSelectPhase("")}>All</button>
+        <button className={selectedPhase === "5" ? "active" : ""} onClick={()=>onSelectPhase("5")}>Phase 5</button>
+        <button className={selectedPhase === "4" ? "active" : ""} onClick={()=>onSelectPhase("4")}>Phase 4</button>
+        <button className={selectedPhase === "3" ? "active" : ""} onClick={()=>onSelectPhase("3")}>Phase 3</button>
+        <button className={selectedPhase === "2" ? "active" : ""} onClick={()=>onSelectPhase("2")}>Phase 2</button>
+        <button className={selectedPhase === "1" ? "active" : ""} onClick={()=>onSelectPhase("1")}>Phase 1</button>
       </div>
       <input
         type="text"
         placeholder="Search..."
         onChange={handleSearch}
+        value={searchQuery}
       />
 
       <ul className="cards">{projectListItems}</ul>
--- 05_side_effects_and_data_fetching/project_showcase/src/components/ProjectsContainer.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/components/ProjectsContainer.js
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import ProjectForm from "./ProjectForm";
 import ProjectList from "./ProjectList";
 
 const ProjectsContainer = () => {
   const [projects, setProjects] = useState([]);
-
-  const loadProjects = () => {
-    fetch("http://localhost:4000/projects")
-      .then((res) => res.json())
-      .then((projectsData) => setProjects(projectsData));
-  };
+  const [selectedPhase, setSelectedPhase] = useState("")
+  const [searchQuery, setSearchQuery] = useState("")
+  // const loadProjects = () => {
+  //   fetch("http://localhost:4000/projects")
+  //     .then((res) => res.json())
+  //     .then((projectsData) => setProjects(projectsData));
+  // };
+  
+  useEffect(() => {
+      let url = "http://localhost:4000/projects"
+      if (selectedPhase && searchQuery){
+        url += `?phase=${selectedPhase}&q=${searchQuery}`
+      } else if(selectedPhase){
+        url += `?phase=${selectedPhase}`
+      } else if (searchQuery) {
+        url += `?q=${searchQuery}`
+      }
+      fetch(url)
+        .then((res) => res.json()) // eventually, would like to handle errors here
+        .then((projectsData) => setProjects(projectsData));
+  }, [selectedPhase, searchQuery])
 
   const addProject = (newProject) => {
     console.log("🚀 ~ addProject ~ newProject:", newProject);
@@ -23,7 +38,13 @@ const ProjectsContainer = () => {
       <ProjectForm
         onAddProject={addProject} 
       />
-      <ProjectList onLoadProjects={loadProjects} projects={projects} />
+      <ProjectList  
+        projects={projects} 
+        onSelectPhase={setSelectedPhase}
+        searchQuery={searchQuery}
+        onSearchChange={setSearchQuery}
+        selectedPhase={selectedPhase}
+      />
     </>
   );
 };
